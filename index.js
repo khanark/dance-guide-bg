eba7b2b5cf4be4d06619a3e2a5a2091dc0c595a1
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-01 21:01:52 +0100

    update index.js
    imported database and routes and then loaded them

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const CORS = require('cors');
+const router = require('./config/routes');
+const database = require('./config/database.js');
 
 const app = express();
 const port = 3000;
 
 const start = async () => {
-  await require('./config/database')();
   app.use(express.json());
   app.use(CORS());
+
+  await database();
+  router(app);
+
   app.listen(port, () => console.log(`Server is listening on port ${port}...`));
 };
 
