f4a73cdab20b82d91ad1cfd5a658921c04578f2a
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-02 22:05:18 +0100

    add global handler to the routes

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,7 @@
+const globalErrorHandler = require('../middlewares/error_handler');
+
 module.exports = app => {
   app.use('/users');
   app.use('/dance/schools');
+  app.use(globalErrorHandler());
 };
