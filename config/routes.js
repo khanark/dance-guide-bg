84a952600ab622800bd8a2ef88b0563d214606b0
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-03 22:45:34 +0100

    update routes

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,8 @@
 const globalErrorHandler = require('../middlewares/error_handler');
+const userController = require('../controllers/user_controller');
 
 module.exports = app => {
-  app.use('/users');
-  app.use('/dance/schools');
+  app.use('/users', userController);
+  // app.use('/dance/schools');
   app.use(globalErrorHandler());
 };
